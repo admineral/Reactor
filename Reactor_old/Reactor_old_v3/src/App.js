3930a9d87381d82a813c46676ec0c64585392a05
Elias <50579369+admineral@users.noreply.github.com>
2023-05-19 12:06:19 +0200

    Sandpack Revert Button implemented and initial code as seperate file inititalcode.js

--- Reactor_old/Reactor_old_v3/src/App.js
+++ Reactor_old/Reactor_old_v3/src/App.js
@@ -4,106 +4,9 @@ import { SandpackProvider, SandpackLayout, SandpackCodeEditor, SandpackPreview }
 import { monokaiPro } from "@codesandbox/sandpack-themes";
 import { autocompletion, completionKeymap } from "@codemirror/autocomplete";
 
+import { initialCode } from "./InitialCode";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
-
-const initialCode = `import React, { useState } from 'react';
-import { Button, Typography, Container, Box, Fade, Tooltip } from '@mui/material';
-import styled from '@emotion/styled';
-import InfoIcon from '@mui/icons-material/Info';
-import { keyframes } from '@emotion/react';
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
-
-const fadeIn = keyframes\`
-  from {
-    opacity: 0;
-  }
-  to {
-    opacity: 1;
-  }
-\`;
-
-const StyledContainer = styled(Container)({
-  backgroundImage: 'linear-gradient(to right, #6a11cb, #2575fc)',
-  minHeight: '100vh',
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  alignItems: 'center',
-  textAlign: 'center',
-  padding: '16px',
-  color: 'white',
-  animation: \`\${fadeIn} 2s ease-in-out\`,
-});
-
-const StyledButton = styled(Button)({
-  marginTop: '16px',
-});
-
-function App() {
-  const navigate = useNavigate();
-  const [buttonClicked, setButtonClicked] = useState(false);
-
-  const handleButtonClick = () => {
-    setButtonClicked(true);
-    navigate('/about');
-  };
-
-  return (
-    <StyledContainer>
-      <Fade in={true} timeout={2000}>
-        <Typography variant="h4" component="h1">
-          Welcome to Our Website
-        </Typography>
-      </Fade>
-      <Fade in={true} timeout={3000}>
-        <Typography variant="h6">
-          We provide top-notch services and solutions for our customers. Explore our offerings and find the best fit for your needs!
-        </Typography>
-      </Fade>
-      <Box mt={2}>
-        <Tooltip title={buttonClicked ? 'You have clicked the button!' : 'Click to learn more!'} arrow>
-          <StyledButton
-            variant="contained"
-            color="primary"
-            endIcon={<InfoIcon />}
-            onClick={handleButtonClick}
-          >
-            {buttonClicked ? 'Thanks for clicking!' : 'Learn More'}
-          </StyledButton>
-        </Tooltip>
-      </Box>
-    </StyledContainer>
-  );
-}
-
-function About() {
-  return (
-    <StyledContainer>
-      <Typography variant="h4" component="h1">
-        About Us
-      </Typography>
-      <Typography variant="h6">
-        This is the About page. Here you can learn more about us!
-      </Typography>
-    </StyledContainer>
-  );
-}
-
-function Main() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-export default Main;
-`;
-
 
 
 function App() {
@@ -206,6 +109,8 @@ function App() {
   }, [messages]);
 
 
+
+
   return (
 
     <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
@@ -256,7 +161,7 @@ function App() {
                 showOpenInCodeSandbox={false}
                 
                 actionsChildren={
-                  <button onClick={() => window.alert("not implemented yet")}>
+                  <button onClick={revertCode}>
                     Revert Code
                   </button>
                 }
@@ -317,37 +222,28 @@ function App() {
               <strong>ChatGPT:</strong> thinking...
             </div>
           )}
-        </Box>
-        <form onSubmit={handleChatSubmit}>
-          <Box display="flex" p={1}>
-            <TextField
-              fullWidth
-              value={chatInput}
-              onChange={(e) => setChatInput(e.target.value)}
-              variant="outlined"
-              size="small"
-              label="Type your message"
-            />
-            <Button type="submit" variant="contained" color="primary" style={{ marginLeft: "1rem" }}>
-              Send
-            </Button>
-            <Button
-              variant="contained"
-              color="secondary"
-              style={{ marginLeft: "1rem" }}
-              onClick={revertCode}
-            >
-              Revert Code
-            </Button>
           </Box>
-        </form>
-      </Box>
-    </div>
-  );
-}
+          <form onSubmit={handleChatSubmit}>
+            <Box display="flex" p={1}>
+              <TextField
+                fullWidth
+                value={chatInput}
+                onChange={(e) => setChatInput(e.target.value)}
+                variant="outlined"
+                size="small"
+                label="Type your message"
+              />
+              <Button type="submit" variant="contained" color="primary" style={{ marginLeft: "1rem" }}>
+                Send
+              </Button>
+            </Box>
+          </form>
+          </Box>
+          </div>
+          );
+          }
 
 export default App;
-
 
 
 
---
+++ Reactor_old/Reactor_old_v3/src/InitialCode.js
@@ -0,0 +1,119 @@
+// InitialCode.js
+
+
+
+
+export const initialCode =`import React, { useState } from 'react';
+import { Button, Typography, Container, Box, Fade, Tooltip } from '@mui/material';
+import styled from '@emotion/styled';
+import InfoIcon from '@mui/icons-material/Info';
+import HomeIcon from '@material-ui/icons/Home';
+import { keyframes } from '@emotion/react';
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+
+const fadeIn = keyframes\`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+\`;
+
+const StyledContainer = styled(Container)({
+  backgroundImage: 'linear-gradient(to right, #6a11cb, #2575fc)',
+  minHeight: '100vh',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  textAlign: 'center',
+  padding: '16px',
+  color: 'white',
+  animation: \`\${fadeIn} 2s ease-in-out\`,
+});
+
+const StyledButton = styled(Button)({
+  marginTop: '16px',
+});
+
+function App() {
+  const navigate = useNavigate();
+  const [buttonClicked, setButtonClicked] = useState(false);
+
+  const handleButtonClick = () => {
+    setButtonClicked(true);
+    navigate('/about');
+  };
+
+  return (
+    <StyledContainer>
+      <Fade in={true} timeout={2000}>
+        <Typography variant="h4" component="h1">
+          Welcome to Our Website
+        </Typography>
+      </Fade>
+      <Fade in={true} timeout={3000}>
+        <Typography variant="h6">
+          We provide top-notch services and solutions for our customers. Explore our offerings and find the best fit for your needs!
+        </Typography>
+      </Fade>
+      <Box mt={2}>
+        <Tooltip title={buttonClicked ? 'You have clicked the button!' : 'Click to learn more!'} arrow>
+          <StyledButton
+            variant="contained"
+            color="primary"
+            endIcon={<InfoIcon />}
+            onClick={handleButtonClick}
+          >
+            {buttonClicked ? 'Thanks for clicking!' : 'Learn More'}
+          </StyledButton>
+        </Tooltip>
+      </Box>
+    </StyledContainer>
+  );
+}
+
+function About() {
+  const navigate = useNavigate();
+  const handleReturnHomeClick = () => {
+    navigate('/');
+  };
+
+  return (
+    <StyledContainer>
+      <Typography variant="h4" component="h1">
+        About Us
+      </Typography>
+      <Typography variant="h6">
+        This is the About page. Here you can learn more about us!
+      </Typography>
+      <Box mt={2}>
+        <Tooltip title="Return to Home" arrow>
+          <StyledButton
+            variant="contained"
+            color="primary"
+            endIcon={<HomeIcon />}
+            onClick={handleReturnHomeClick}
+          >
+            Return to Home
+          </StyledButton>
+        </Tooltip>
+      </Box>
+    </StyledContainer>
+  );
+}
+
+function Main() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Main;
+`;
