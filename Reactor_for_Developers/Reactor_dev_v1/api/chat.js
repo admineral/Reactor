1cdf2861af8b35961002a41754e74d2abd8e7e83
admineral <50579369+admineral@users.noreply.github.com>
2023-09-20 16:00:28 +0200

    cant find the bug, please wait with cloning

--- Reactor_for_Developers/Reactor_dev_v1/api/chat.js
+++ Reactor_for_Developers/Reactor_dev_v1/api/chat.js
@@ -1,4 +1,3 @@
-// /api/chatGpt.js
 import OpenAI from 'openai';
 import { OpenAIStream, StreamingTextResponse } from 'ai';
 
@@ -8,22 +7,34 @@ const openai = new OpenAI({
 
 export async function POST(req, res) {
   try {
-    const { prompt } = await req.json();
-    
-    // Create a completion using OpenAI
+    const { messages } = await req.json();
+
+    // Create a chat completion using OpenAI
     const response = await openai.completions.create({
-      model: 'gpt-3.5-turbo-instruct', // or any other compatible model you wish to use
+      model: 'gpt-3.5-turbo-instruct',
       stream: true,
-      prompt,
+      messages,
     });
-    
+
     // Transform the response into a readable stream
     const stream = OpenAIStream(response);
-    
+
     // Return a StreamingTextResponse to the client
     return res.status(200).send(new StreamingTextResponse(stream));
 
   } catch (error) {
-    return res.status(500).send({ error: `Failed to get response from OpenAI: ${error.message}` });
+    if (error.name === "APIError") {
+      console.error(`OpenAI API returned an API Error: ${error.message}`);
+      return res.status(500).send({ error: `API Error from OpenAI: ${error.message}` });
+    } else if (error.name === "APIConnectionError") {
+      console.error(`Failed to connect to OpenAI API: ${error.message}`);
+      return res.status(500).send({ error: `Connection error with OpenAI: ${error.message}` });
+    } else if (error.name === "RateLimitError") {
+      console.error(`OpenAI API request exceeded rate limit: ${error.message}`);
+      return res.status(429).send({ error: `Rate limit exceeded: ${error.message}` });
+    } else {
+      console.error(`Failed to get response from OpenAI: ${error.message}`);
+      return res.status(500).send({ error: `Unknown error: ${error.message}` });
+    }
   }
 }
