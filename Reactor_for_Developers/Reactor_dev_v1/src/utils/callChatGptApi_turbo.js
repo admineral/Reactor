40b35494129844fedaf3513a0ede6b1f7c1934ef
admineral <50579369+admineral@users.noreply.github.com>
2023-09-20 10:50:31 +0200

    fix

--- Reactor_for_Developers/Reactor_dev_v1/src/utils/callChatGptApi_turbo.js
+++ Reactor_for_Developers/Reactor_dev_v1/src/utils/callChatGptApi_turbo.js
@@ -1,7 +1,6 @@
 import { Configuration, OpenAIApi } from 'openai-edge';
-import { OpenAIStream, StreamingTextResponse } from 'ai';
+import { AIStream } from 'ai';
 
-// Create an OpenAI API client
 const config = new Configuration({
   apiKey: process.env.REACT_APP_API_KEY,
 });
@@ -9,7 +8,6 @@ const openai = new OpenAIApi(config);
 
 export const runtime = 'edge';
 
-// Utility function to check if a string is valid JSON
 const isJSON = (str) => {
     try {
         JSON.parse(str);
@@ -19,29 +17,21 @@ const isJSON = (str) => {
     }
 };
 
+function parseOpenAIStream(): AIStreamParser {
+  return data => {
+    if (isJSON(data)) {
+      const parsedData = JSON.parse(data);
+      return parsedData.choices?.[0]?.delta?.content || "";
+    }
+    return "";
+  }
+}
+
 export const fetchChatGptResponseTurbo = async (code, chatInput, updateUI) => {
   try {
     const prompt = `Here is my current React code snippet: \n\n${code}\n\n . I need to make the following changes or additions to my code: ${chatInput}. \n\n
-      Could you provide a detailed explanation understandable to a five-year-old but no code, and then the COMPLETE UPDATED CODE that incorporates these changes or additions? Also,if there are any new or updated dependencies needed, please list them out, each on a new line. If there are no new or updated dependencies, please DO NOT write anything in the dependencies section, leave it COMPLETELY EMPTY.
-
-      Note: Please NEVER show code in the explanation, and ensure to format your response as follows: explanation, then code, then dependencies. Please answer in this format: 
-        \n\n
-      [Your short explanation without code here].
-
-      {__CodeStart__}
-
-      [Your entire React code here]
-
-      {__CodeEnd__}
-
-      {__DependenciesStart__}
-
-      [If there are any new or updated dependencies, list them here. If there are none, write __none__ ]
-
-      {__DependenciesEnd__}`
-    ;
-
-    console.log('Sending OpenAI request...');
+    Could you provide a detailed explanation understandable to a five-year-old but no code, and then the COMPLETE UPDATED CODE that incorporates these changes or additions? Also, if there are any new or updated dependencies needed, please list them out, each on a new line. If there are no new or updated dependencies, please DO NOT write anything in the dependencies section, leave it COMPLETELY EMPTY. Note: Please NEVER show code in the explanation, and ensure to format your response as follows: explanation, then code, then dependencies. Please answer in this format: \n\n
+    [Your short explanation without code here].{__CodeStart__}[Your entire React code here]{__CodeEnd__}{__DependenciesStart__}[If there are any new or updated dependencies, list them here. If there are none, write __none__ ]{__DependenciesEnd__}`;
     const response = await openai.createCompletion({
       model: 'gpt-3.5-turbo-instruct',
       prompt,
@@ -50,73 +40,55 @@ export const fetchChatGptResponseTurbo = async (code, chatInput, updateUI) => {
       top_p: 1,
       frequency_penalty: 0,
       presence_penalty: 0,
-      stream: true // Enable stream response
+      stream: true
     });
-    if (!response || !response.ok) {
+
+    if (!response.ok) {
       throw new Error('Invalid response from OpenAI');
     }
-    console.log('Response from OpenAI:', response);
-
-    console.log('Stream started...');
-    const stream = OpenAIStream(response);
-    console.log('Stream from OpenAIStream:', stream);
 
     let buffer = '';
-    let extractedCode = null;
-    let extractedDependencies = null;
-    let extractedAnswer = null;
-    let codeStartIndex = -1;
 
-    for await (const chunk of stream) {
-      console.log('Chunk from stream:', chunk);
-      const lines = chunk.split('\n');
+    const openAIStream = AIStream(response, parseOpenAIStream(), {
+      onStart: async () => {
+        console.log('Stream started');
+      },
+      onCompletion: async (completion) => {
+        buffer += completion;
+        updateUI(completion);
+      },
+      onFinal: async (completion) => {
+        console.log("Stream completed");
 
-      for (const line of lines) {
-        if (line.startsWith('data:')) {
-          const jsonLine = line.slice('data:'.length);
-          if (isJSON(jsonLine)) {
-            const parsedChunk = JSON.parse(jsonLine);
-            const content = parsedChunk.choices?.[0]?.delta?.content;
-
-            if (content) {
-              // Add content to the buffer
-              buffer += content;
-              // Send content to UI
-              updateUI(content);
+        let extractedCode, extractedDependencies, extractedAnswer;
+        const answerEndCodeStartIndex = buffer.indexOf('{__CodeStart__}');
+        let dependenciesStartIndex = buffer.indexOf('{__DependenciesStart__}');
+        
+        if (answerEndCodeStartIndex !== -1) {
+          extractedAnswer = buffer.substring(0, answerEndCodeStartIndex).trim();
+          const codeEndIndex = buffer.indexOf('{__CodeEnd__}', answerEndCodeStartIndex);
+          if (codeEndIndex !== -1) {
+            const codeStartIndex = answerEndCodeStartIndex + '{__CodeStart__}'.length;
+            extractedCode = buffer.substring(codeStartIndex, codeEndIndex).trim();
+          }
+          if (dependenciesStartIndex !== -1) {
+            const dependenciesEndIndex = buffer.indexOf('{__DependenciesEnd__}', dependenciesStartIndex);
+            if (dependenciesEndIndex !== -1) {
+              dependenciesStartIndex += '{__DependenciesStart__}'.length;
+              extractedDependencies = buffer.substring(dependenciesStartIndex, dependenciesEndIndex).trim();
             }
           }
+        } else {
+          extractedAnswer = buffer;
         }
+
+        return { answer: extractedAnswer, code: extractedCode, dependencies: extractedDependencies };
       }
-    }
+    });
 
-     // After the stream has ended
-    const answerEndCodeStartIndex = buffer.indexOf('{__CodeStart__}');
-    let dependenciesStartIndex = buffer.indexOf('{__DependenciesStart__}');  // <-- Declare once here
-    if (answerEndCodeStartIndex !== -1) {
-      extractedAnswer = buffer.substring(0, answerEndCodeStartIndex).trim();
-      const codeEndIndex = buffer.indexOf('{__CodeEnd__}', answerEndCodeStartIndex);
-      if (codeEndIndex !== -1) {
-        codeStartIndex = answerEndCodeStartIndex + '{__CodeStart__}'.length;
-        extractedCode = buffer.substring(codeStartIndex, codeEndIndex).trim();
-      }
-      if (dependenciesStartIndex !== -1) {
-        const dependenciesEndIndex = buffer.indexOf('{__DependenciesEnd__}', dependenciesStartIndex);
-        if (dependenciesEndIndex !== -1) {
-          dependenciesStartIndex = dependenciesStartIndex + '{__DependenciesStart__}'.length;  // <-- Update value here
-          extractedDependencies = buffer.substring(dependenciesStartIndex, dependenciesEndIndex).trim();
-        }
-      }
-    } else {
-      extractedAnswer = buffer;
-    }
-
-    console.log('Stream ended...');
-    // return extractedAnswer, extractedCode, and extractedDependencies
-    return { answer: extractedAnswer, code: extractedCode, dependencies: extractedDependencies };
-
-
+    // Now you can consume the openAIStream as needed
+    // ... 
   } catch (error) {
-    // Handle general errors
     console.log(`Error occurred while fetching GPT-3 response: ${error}`);
     throw error;
   }
